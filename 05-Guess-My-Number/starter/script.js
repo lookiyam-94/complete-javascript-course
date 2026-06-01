58f28645fdfc8c6476544bba828fdf13d7632902
lookiyam-94 <vincenzo.calafiore.techart@gmail.com>
2026-06-01 10:42:34 +0200

    Refactor guess handling in "Guess My Number" game; improved feedback messages for incorrect guesses and streamlined win state logic.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,12 +1,5 @@
 'use strict';
 
-/*
-document.querySelector('.message').textContent = '🎉 Correct Number!';
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-document.querySelector('.guess').value = 23;
-*/
-
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 document.querySelector('.number').textContent = `${secretNumber}cm`;
 
@@ -17,52 +10,41 @@ let highscore = 0;
 const checkGuess = function () {
   const guess = Number(document.querySelector('.guess').value);
 
-//no input
+// no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ oh come on!';
 
-//correct number
+// correct number
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.message').textContent = '🥳Correct Number!';
     document.querySelector('.number').textContent =`${secretNumber}cm`;
 
     if (score > highscore) {
-      highscore = score; 
+      highscore = score;
       document.querySelector('.highscore').textContent = highscore;
-    };
-    
-    //wing state change
+    }
+
+// win state change
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
-//too high
-  } else if (guess > secretNumber) {
-    if (score > 1){
-        document.querySelector('.message').textContent = 'Nah! Too high!';
-        score--;
-        document.querySelector('.score').textContent = score;
+// incorrect guess
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? '🥲Nah! Too high!' : '😭Nah! Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
     } else {
-        document.querySelector('.message').textContent = 'Set up a date and let me show you!';
-        document.querySelector('.score').textContent = 0;
-
-        document.querySelector('.again').style.backgroundColor = '#87CEEB';
-    }
-
-//too low
-  } else if (guess < secretNumber) {
-    if (score > 1){
-        document.querySelector('.message').textContent = 'Nah! Too high!';
-        score--;
-        document.querySelector('.score').textContent = score;
-    } else {
-        document.querySelector('.message').textContent = 'Set up a date and let me show you!';
-        document.querySelector('.score').textContent = 0;
-        document.querySelector('.again').style.backgroundColor = '#87CEEB';
+      document.querySelector('.message').textContent =
+        'Set up a date and let me show you!';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.again').style.backgroundColor = '#87CEEB';
     }
   }
 };
 
-//check button functionality
+// check button functionality
 document.querySelector('.check').addEventListener('click', checkGuess);
 
 //enter key functionality
